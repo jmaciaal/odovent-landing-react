eb0512262e35332ead616c64f921942a560e8fea
jmaciaal <j.maciaal@gmail.com>
2025-05-04 23:38:53 -0600

    Se agrego favicon y metadatos para SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
+import Head from "next/head";
 import { PlasmicRootProvider, PlasmicComponent } from "@plasmicapp/loader-nextjs";
 import { PLASMIC } from "../plasmic-init";
 
 export default function Home() {
   return (
-    <PlasmicRootProvider loader={PLASMIC}>
-      <PlasmicComponent component="Home" />
-    </PlasmicRootProvider>
+    <>
+      <Head>
+        <title>Odovent Depósito Dental | Suministros para dentistas en Mexicali</title>
+        <meta name="description" content="Venta de insumos dentales en Mexicali. Resinas, anestesias y kits para estudiantes. Atención rápida y precios preferenciales." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Odovent Depósito Dental" />
+        <meta property="og:description" content="Suministros para odontólogos y estudiantes en Mexicali." />
+        <meta property="og:image" content="https://odovent-landing.vercel.app/og-image.jpg" />
+        <meta name="author" content="Odovent" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <PlasmicRootProvider loader={PLASMIC}>
+        <PlasmicComponent component="Home" />
+      </PlasmicRootProvider>
+    </>
   );
 }
